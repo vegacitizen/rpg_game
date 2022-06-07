96d19e8c58bb47c04c38137224a06b346ef4c457
vegacitizen <vegacitizen@gmail.com>
2022-06-08 00:19:32 +0900

    apply enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,18 +9,17 @@ public class Enemy : MonoBehaviour
 
     RectTransform hpBar;
 
-    public float height = 1.7f;
+    public float enemyHeight = 1.7f;
 
     // Start is called before the first frame update
     void Start()
     {
-        hpBar = Instantiate(prefabHpBar, canvas.transform).GetComponent<RectTransform>();
+        hpBar = Instantiate(prefabHpBar, canvas.transform).GetComponent<RectTransform>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
-        hpBar.position = _hpBarPos;
+        hpBar.position = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + enemyHeight, 0));
     }
 }
